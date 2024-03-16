0b1cd0db6384183bf4458dd60d2953125a57ff69
WANG <wangqianlf@live.com>
2024-03-16 19:38:22 +0100

    réalisation d'un chronomètre en C#

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,76 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(165, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(26, 0, 26, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 190);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button1.Location = new System.Drawing.Point(62, 269);
+            this.Button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(190, 60);
+            this.Button1.TabIndex = 1;
+            this.Button1.Text = "Commencer";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button2.Location = new System.Drawing.Point(332, 269);
+            this.Button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(190, 60);
+            this.Button2.TabIndex = 2;
+            this.Button2.Text = "Réinitialiser";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Timer1
+            // 
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(70F, 135F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(576, 390);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(26, 25, 26, 25);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Chronomèrtre";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Timer Timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,29 @@ namespace Chronometre
 {
     public partial class Form1 : Form
     {
+        int compteur = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Timer1.Enabled = !Timer1.Enabled;
+            Button1.Text = Timer1.Enabled ? "Arrêter" : "Commencer";
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            compteur++;
+            label1.Text = compteur.ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            compteur = 0;
+            label1.Text = compteur.ToString();
         }
     }
 }
